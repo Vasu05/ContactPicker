1209826c78456be961e1bbe0cd2e4d5d67c1802c
Vasu Chand <Vasu.chand@mobikwik.com>
2019-04-19 14:26:50 +0530

    project setup

---
+++ Eventers/HomePageVC.swift
@@ -0,0 +1,50 @@
+//
+//  VHomePageVC.swift
+//  Eventers
+//
+//  Created by Vasu Chand on 19/04/19.
+//  Copyright © 2019 Vasu Chand. All rights reserved.
+//
+
+import UIKit
+
+class HomePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func requestAccess(completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+        switch CNContactStore.authorizationStatus(for: .contacts) {
+        case .authorized:
+            completionHandler(true)
+        case .denied:
+            showSettingsAlert(completionHandler)
+        case .restricted, .notDetermined:
+            store.requestAccess(for: .contacts) { granted, error in
+                if granted {
+                    completionHandler(true)
+                } else {
+                    DispatchQueue.main.async {
+                        self.showSettingsAlert(completionHandler)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func showSettingsAlert(_ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+        let alert = UIAlertController(title: nil, message: "This app requires access to Contacts to proceed. Would you like to open settings and grant permission to contacts?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Open Settings", style: .default) { action in
+            completionHandler(false)
+            UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!)
+        })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { action in
+            completionHandler(false)
+        })
+        present(alert, animated: true)
+    }
+
+}
+
---
+++ Pods/Target Support Files/Pods-Eventers/Pods-Eventers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Eventers : NSObject
+@end
+@implementation PodsDummy_Pods_Eventers
+@end
